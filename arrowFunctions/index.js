f61d7433018979f550af14e2a4b6f9b3415a9113
Rafael Botelho Cabral <118222979+rafaelbcabral@users.noreply.github.com>
2024-05-14 15:19:45 -0300

    arrowfunctions

---
+++ arrowFunctions/index.js
@@ -0,0 +1,19 @@
+const arrowSum = (a, b) => {
+  return a + b
+}
+
+console.log(`Soma arrow function: ${arrowSum(2, 2)}`)
+
+const subtract = (a, b) => a - b
+
+console.log(`Subtração: ${subtract(5, 2)}`)
+
+const double = n => `O dobro de ${n} é ${n * 2}`
+
+const number = 21
+console.log(`Dobro: ${double(number)}`)
+
+const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta', 'Geffen', 'Morroc']
+
+const startingWithP = towns.filter(town => town[0] === 'P')
+console.log(startingWithP)
